d44487204ce66f89594bd860d0a145c1af849d05
ahmet <ahmet.altun60@gmail.com>
2023-01-17 23:13:49 +0300

    SpecificationPattern hazırlandı

---
+++ SkinetECommerce.Core/Specifications/Concrete/BaseSpecification.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+using SkinetECommerce.Core.Specifications.Abstract;
+
+namespace SkinetECommerce.Core.Specifications.Concrete;
+
+public class BaseSpecification<T>: ISpecification<T>
+{
+    public Expression<Func<T, bool>> Criteria { get; }
+    public List<Expression<Func<T, object>>> Includes { get; } = new();
+
+    public BaseSpecification(Expression<Func<T, bool>> criteria)
+    {
+        Criteria = criteria;
+    }
+
+    protected void AddInclude(Expression<Func<T, object>> includesExpression)
+    {
+        Includes.Add(includesExpression);
+    }
+}
---
+++ SkinetECommerce.DataAccess/Specification/SpecificationEvaluator.cs
@@ -0,0 +1,19 @@
+﻿using SkinetECommerce.Core.Entities.Abstact;
+using SkinetECommerce.Core.Specifications.Abstract;
+
+namespace SkinetECommerce.DataAccess.Specification;
+
+public class SpecificationEvaluator<TEntity> where TEntity: class, IEntity, new()
+{
+    public static IQueryable<TEntity> GetQuery(IQueryable<TEntity> inputQuery, ISpecification<TEntity> specification)
+    {
+        var query = inputQuery;
+        if (specification.Criteria != null)
+        {
+            query = query.Where(specification.Criteria);
+        }
+
+        query = specification.Includes.Aggregate(query, (current, include) => current.Include(include));
+        return query;
+    }
+}
